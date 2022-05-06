5efd8846f848e9237bc257035805f3f00d1e8105
Cleps <104847424+Cleps@users.noreply.github.com>
2022-05-06 11:31:31 -0500

    Add files via upload

--- emprestimo.py
+++ emprestimo.py
@@ -1,4 +1,6 @@
 class Emprestimo():
+    _total_emprestimos = 0
+
     def __init__(self, valor, parcelas, juros, conta):
         self._valor = valor
         self._parcelas = parcelas
@@ -7,6 +9,11 @@ class Emprestimo():
         self._montante = self._valor + (self._valor*juros)
         self._valor_parcela = self._montante / self._parcelas
         conta.depositar(self._valor)
+        Emprestimo._total_emprestimos += 1
+
+    @classmethod
+    def total_emprestimos(cls):
+        return cls._total_emprestimos
 
     def pagar_parcela(self):
         self._conta.sacar(self._valor_parcela)

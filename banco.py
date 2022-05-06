4ca4696470bf7dea9aa84ecab1f1b85a668ae075
Cleps <104847424+Cleps@users.noreply.github.com>
2022-05-06 10:51:44 -0500

    Add files via upload

---
+++ banco.py
@@ -0,0 +1,26 @@
+class Banco:
+    _contas = []
+    def __int__(self):
+        self._contas = []
+        self._caixa_geral = 0
+
+
+    def incluir_conta(self, conta):
+        self._contas.append(conta)
+
+    def remover_conta(self, conta):
+        self._contas.remove(conta)
+
+    def listar_contas(self):
+        caixa_temp = 0
+        for c in self._contas:
+
+            print(f'Extrato da conta {c.numero}')
+            c.extrato.imprime()
+            print(f'Saldo final {c.saldo}')
+            print('---------------------------')
+            caixa_temp += c.saldo
+        self._caixa_geral = caixa_temp
+
+    def imprime_caixa(self):
+        print(f'Total de dinheiro no banco: {self._caixa_geral}')
---
+++ main.py
@@ -0,0 +1,21 @@
+from conta import Conta
+from banco import Banco
+
+
+c1 = Conta(1, 'fulano', 1000, 123)
+c2 = Conta(2, 'cicrano', 2000, 123)
+c3 = Conta(3, 'carinha', 3000, 123)
+
+bb = Banco()
+
+bb.incluir_conta(c1)
+bb.incluir_conta(c2)
+bb.incluir_conta(c3)
+
+c1.depositar(1000)
+c1.sacar(500)
+
+c2.depositar(1000)
+
+bb.listar_contas()
+bb.imprime_caixa()
